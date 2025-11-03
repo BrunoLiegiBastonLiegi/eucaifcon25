c9592855eb099c5876a426652c94699db46c2ceb
BrunoLiegiBastonLiegi <andreapapaluca@gmail.com>
2025-11-03 13:06:08 +0100

    fix: new qbuild

---
+++ dist/assets/slidev/default-BGjluQbS.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-C9I5pWO7.js";import{_ as r}from"../index-kkzzkbQG.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
